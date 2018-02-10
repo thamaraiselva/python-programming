470dabfd4988307ddfd969d5d8f5e72368fa4b8e
thamaraiselva <thamaraiselvan461@gmail.com>
2018-02-10 16:37:51 +0530

    Update and rename vowel.py to l.py

---
+++ l.py
@@ -0,0 +1,6 @@
+l = input("Input a letter of the alphabet: ")
+
+if l in ('5'):
+	print("15")
+else:
+	print("3") 
--- vowel.py
+++
@@ -1,9 +0,0 @@
-l = input("Input a letter of the alphabet: ")
-
-if l in ('a', 'e', 'i', 'o', 'u'):
-	print("Vowel")
-else:
-	print("Consonant") 
-	
-
- 
